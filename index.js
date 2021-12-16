8892c2d11ad0b36c72faf0edfcc17a85f471beb5
Steven Dombrowski <dombrowskis153@yahoo.com>
2021-12-16 00:48:28 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink){
+    this.drink = drink;
+    this.food = food;
+    }
+
+}
+let bfast = new Breakfast("eggs", "juice");
+
+class Lunch {
+constructor(salad, soup, drink){
+   this.salad = salad;
+   this.soup = soup;
+   this.drink = drink;
+ }
+}
+let lunch = new Lunch("side salad", "broccoli cheddar soup", "iced tea")
+
+class Dinner {
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.dessert = dessert
+    }
+}
+let dinner = new Dinner("balsamic salad",
+"consomme",
+"filet mignon",
+"cheesecake")
